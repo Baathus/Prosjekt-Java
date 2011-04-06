5db7ad0f9773619f7a182903657f9f842ca87c63
Mikael <mikaelj_89@hotmail.com>
2011-04-06 11:04:36 +0200

    laget datanoden

---
+++ Data.java
@@ -0,0 +1,44 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class Data {
+	private Calendar dato;
+	private double mintemp;
+	private double maxtemp;
+	private double nedbør;
+	
+	public Data(Calendar d, double min, double max, double ned)
+	{
+		dato = d;
+		mintemp = min;
+		maxtemp = max;
+		nedbør = ned;
+	}
+	
+	public double getMinTemp()
+	{
+		return mintemp;
+	}
+	
+	public double getMaxTemp()
+	{
+		return maxtemp;
+	}
+	
+	public double getNedbør()
+	{
+		return nedbør;
+	}
+	
+	public Calendar getDato()
+	{
+		return dato;
+	}
+	
+	public String toString()
+	{
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyy");
+		return sdf.format(dato.getTime()) +"\t"+mintemp+"\t"+maxtemp+"\t"+nedbør;
+	}
+}
